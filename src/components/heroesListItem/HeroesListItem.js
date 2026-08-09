bd9117b04b302f6eee2b4e79634c1e23e950e75c
DimaManko <mankonat@gmail.com>
2026-08-09 17:41:29 +0300

    first commit

---
+++ src/components/heroesListItem/HeroesListItem.js
@@ -0,0 +1,48 @@
+const HeroesListItem = ({ hero, deleteHero }) => {
+  const { id, name, description, element } = hero;
+  let elementClassName;
+
+  switch (element) {
+    case "fire":
+      elementClassName = "bg-danger bg-gradient";
+      break;
+    case "water":
+      elementClassName = "bg-primary bg-gradient";
+      break;
+    case "wind":
+      elementClassName = "bg-success bg-gradient";
+      break;
+    case "earth":
+      elementClassName = "bg-secondary bg-gradient";
+      break;
+    default:
+      elementClassName = "bg-warning bg-gradient";
+  }
+
+  return (
+    <li
+      className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}
+    >
+      <img
+        src="https://t4.ftcdn.net/jpg/14/20/16/23/360_F_1420162341_QYUbE0Zlxv1bJpprhXmEOBrThAdQfu9i.jpg"
+        className="img-fluid w-25 d-inline"
+        alt="unknown hero"
+        style={{ objectFit: "cover" }}
+      />
+      <div className="card-body">
+        <h3 className="card-title">{name}</h3>
+        <p className="card-text">{description}</p>
+      </div>
+      <span className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
+        <button
+          onClick={() => deleteHero(id)}
+          type="button"
+          className="btn-close btn-close"
+          aria-label="Close"
+        ></button>
+      </span>
+    </li>
+  );
+};
+
+export default HeroesListItem;
